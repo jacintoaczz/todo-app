07b412e06086b09dda41e7004631329dbee8f7e9
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-05 23:33:26 -0400

    Obteniendo los datos desde la store

--- src/app/features/todos/todo-list/todo-list.component.ts
+++ src/app/features/todos/todo-list/todo-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store/app.reducers';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  todoList$!: Observable<Todo[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _store: Store<AppState>) {
+    this.todoList$ = _store.select('todos');
   }
 
+  ngOnInit(): void {}
 }
