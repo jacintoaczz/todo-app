eaef657daa925896058c483e91da9c2e52b6420c
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-05 23:30:01 -0400

    Implemetadas acciones para crear to-dos usando la store

--- src/app/features/todos/todo-add/todo-add.component.ts
+++ src/app/features/todos/todo-add/todo-add.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { agregar } from '../store/todo.actions';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
-  styleUrls: ['./todo-add.component.scss']
+  styleUrls: ['./todo-add.component.scss'],
 })
 export class TodoAddComponent implements OnInit {
+  txtInput!: FormControl;
 
-  constructor() { }
+  constructor(private _store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.txtInput = new FormControl('', Validators.required);
   }
 
+  public agregarTodo() {
+    if (this.txtInput.invalid) {
+      return;
+    }
+
+    const texto = this.txtInput.value;
+    this._store.dispatch(agregar({ texto }));
+  }
 }
