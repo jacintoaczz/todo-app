06f519f829d7cffc4e2638e2719e4942fdf15441
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-05 23:30:24 -0400

    Creado componente

--- src/app/features/todos/todo-item/todo-item.component.ts
+++ src/app/features/todos/todo-item/todo-item.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
