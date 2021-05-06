a4e0df30716f27f20c0bd422184754ff50dd568c
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-06 08:48:51 -0400

    Implementada la accion para actualizar el estado de todos los to-dos.
    [Funcionalidad OK!]

--- src/app/features/todos/todo/todo.component.ts
+++ src/app/features/todos/todo/todo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { actualizarTodos } from '../store/todo.actions';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  completado = false;
 
-  constructor() { }
+  constructor(private _store: Store<AppState>) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public togglearTodo() {
+    this.completado = !this.completado;
+    const estado = this.completado;
+
+    this._store.dispatch(actualizarTodos({ estado }));
   }
-
 }
