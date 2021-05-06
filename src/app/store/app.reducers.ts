7d3eaa5bb18f1ae068cd2ff248f4ea77e2bbcd8d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-06 12:17:23 -0400

    Organizacion de los reducers en una constante que engloba el estado

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,5 +1,16 @@
+import { ActionReducerMap } from '@ngrx/store';
 import { Todo } from '../features/todos/model/todo.model';
+import * as fromTodo from '../features/todos/store/todo.reducer';
+import * as fromFilter from '../store/filters/filter.reducer';
 
 export interface AppState {
   todos: Todo[];
+  filtro: string;
 }
+
+// Creamos una constante para relacionar el estado con sus respectivos reducers y asi
+// importar dichos valores en el StoreModule de manera mas facil.
+export const appReducers: ActionReducerMap<AppState> = {
+  todos: fromTodo.todoReducer,
+  filtro: fromFilter.filtroReducer,
+};
